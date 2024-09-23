76c4fa1b0a8c93bb3ed6365b34ef7a156a0a655c
doaa <doaazakaria29@gmail.com>
2024-09-23 23:33:34 +0300

    add Shift model

---
+++ AttendanceSystem/Models/Shift.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AttendanceSystem.Models
+{
+    public class Shift
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        public int Num { get; set; }
+        public  DateTime From { get; set; }
+        public DateTime To { get; set; }
+
+        public ICollection<Employee>? Employees { get; set; } = new List<Employee>();
+    }
+}
