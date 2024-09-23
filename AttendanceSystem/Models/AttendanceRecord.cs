4b72ec576a8ecd2302bb08ea3363f8535077fc9f
doaa <doaazakaria29@gmail.com>
2024-09-23 23:31:41 +0300

    add AttendanceRecord model

---
+++ AttendanceSystem/Models/AttendanceRecord.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AttendanceSystem.Models
+{
+    public class AttendanceRecord
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        public DateTime? Date { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        public string OutStatus { get; set; }
+        public string InStatus { get; set; }
+        public string? Note { get; set; }
+
+        [Required]
+        public int EmployeeId { get; set; }
+        [ForeignKey("EmployeeId")]
+        public Employee Employee { get; set; }
+    }
+}
