e9f72bcac5da0db9dc939d860bbd672a89b86980
doaa <doaazakaria29@gmail.com>
2024-09-24 00:42:19 +0300

    adding IRepository

---
+++ AttendanceSystem/Repositoty/IRepository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace Repository.IRepository
+{
+	public interface IRepository<T> where T : class
+	{
+		// T-Category
+		IEnumerable<T> GetAll(Expression<Func<T, bool>>? Filter = null, string? includeproperties =null);
+		T Get(Expression<Func<T, bool>> Filter, string? includeproperties=null,bool tracked=false);
+		void Add(T entity);
+		void Remove(T entity);
+		void RemoveRange(IEnumerable<T> entity);
+	
+	}
+}
