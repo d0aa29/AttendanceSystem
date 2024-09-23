130be40854677afaee3458954138b7c1a2ec8b1a
doaa <doaazakaria29@gmail.com>
2024-09-24 00:41:36 +0300

    adding UnitOfWork

---
+++ AttendanceSystem/Repositoty/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using AttendanceSystem.Data;
+using Repository.IRepository;
+
+namespace Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+
+		public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+       
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+       
+
+   
+    }
+}
