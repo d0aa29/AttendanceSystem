dae411208d2488a9d6dfa07e4d7f98b52ae4d4d2
doaa <doaazakaria29@gmail.com>
2024-09-23 23:32:53 +0300

    add LeaveRequest Model

---
+++ AttendanceSystem/Models/LeaveRequest.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AttendanceSystem.Models
+{
+    public class LeaveRequest
+    { 
+
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string Reason { get; set; }
+        public string ApprovalStatus { get; set; } = "pending...";
+
+        [Required]
+        public int EmployeeId { get; set; }
+        [ForeignKey("EmployeeId")]
+        public Employee Employee { get; set; }
+    }
+}
