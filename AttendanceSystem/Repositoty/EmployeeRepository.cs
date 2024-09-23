f6f2ea9b777ef42821ec3adc55b76296ced1f44d
doaa <doaazakaria29@gmail.com>
2024-09-24 00:46:25 +0300

    adding IEmployeeRepository and Employee

---
+++ AttendanceSystem/Repositoty/EmployeeRepository.cs
@@ -0,0 +1,21 @@
+﻿using AttendanceSystem.Data;
+using AttendanceSystem.Models;
+using AttendanceSystem.Repositoty.IRepository;
+using Repository;
+
+namespace AttendanceSystem.Repositoty
+{
+    public class EmployeeRepository : Repository<Employee>, IEmployeeRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public EmployeeRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Employee employee)
+        {
+            _db.Employees.Update(employee);
+        }
+    }
+}
