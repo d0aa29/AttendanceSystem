687e2ea144cc1ad100854ce4b5e1cb46214abbee
doaa <doaazakaria29@gmail.com>
2024-09-23 23:38:06 +0300

    add ApplicationDbContext

---
+++ AttendanceSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using AttendanceSystem.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AttendanceSystem.Data
+{
+    public class ApplicationDbContext:IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<AttendanceRecord> AttendanceRecords { get; set; }
+        public DbSet<Shift> Shifts { get; set; }
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+
+
+    }
+}
