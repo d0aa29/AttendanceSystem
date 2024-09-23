9042e07abe765416ca870d0eb495822b8825951c
doaa <doaazakaria29@gmail.com>
2024-09-24 00:59:48 +0300

    Adding all Repositories To UnitOfWork

--- AttendanceSystem/Repositoty/IRepository/IUnitOfWork.cs
+++ AttendanceSystem/Repositoty/IRepository/IUnitOfWork.cs
@@ -1,9 +1,19 @@
 ﻿
 
+using AttendanceSystem.Repositoty.IRepository;
+
 namespace Repository.IRepository
 {
     public interface IUnitOfWork
     {
+        public IAttendanceRecordRepository AttendanceRecord { get; }
+
+        public IDepartmentRepository Department { get; }
+        public IEmployeeRepository Employee { get; }
+
+        public ILeaveRequestRepository LeaveRequest { get; }
+        public IShiftRepository Shift { get; }
+
 		void Save();
     }
 }
--- AttendanceSystem/Repositoty/UnitOfWork.cs
+++ AttendanceSystem/Repositoty/UnitOfWork.cs
@@ -1,4 +1,7 @@
 ﻿using AttendanceSystem.Data;
+using AttendanceSystem.Models;
+using AttendanceSystem.Repositoty;
+using AttendanceSystem.Repositoty.IRepository;
 using Repository.IRepository;
 
 namespace Repository
@@ -6,13 +9,22 @@ namespace Repository
     public class UnitOfWork : IUnitOfWork
     {
         private readonly ApplicationDbContext _db;
-
-		public UnitOfWork(ApplicationDbContext db)
+        public IAttendanceRecordRepository AttendanceRecord { get; private set; }
+        public IDepartmentRepository Department { get; private set; }
+        public IEmployeeRepository Employee { get; private set; }
+        public ILeaveRequestRepository LeaveRequest { get; private set; }
+        public IShiftRepository Shift { get; private set; }
+        public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
-       
+            AttendanceRecord=new AttendanceRecordRepository(db);
+            Department=new DepartmentRepository(db);
+            Employee=new EmployeeRepository(db);
+            LeaveRequest=new LeaveRequestRepository(db);
+            Shift = new ShiftRepository(db);
         }
 
+    
         public void Save()
         {
             _db.SaveChanges();
