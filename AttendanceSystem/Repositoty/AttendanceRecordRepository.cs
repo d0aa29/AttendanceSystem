edb810d05ba67a5d7e6d42aa65c76b055fe4df0e
doaa <doaazakaria29@gmail.com>
2024-09-24 00:44:45 +0300

    adding IAttendanceRecordRepository and AttendanceRecord

---
+++ AttendanceSystem/Repositoty/AttendanceRecordRepository.cs
@@ -0,0 +1,21 @@
+﻿using AttendanceSystem.Data;
+using AttendanceSystem.Models;
+using AttendanceSystem.Repositoty.IRepository;
+using Repository;
+
+namespace AttendanceSystem.Repositoty
+{
+    public class AttendanceRecordRepository : Repository<AttendanceRecord>, IAttendanceRecordRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public AttendanceRecordRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(AttendanceRecord record)
+        {
+            _db.AttendanceRecords.Update(record);
+        }
+    }
+}
---
+++ AttendanceSystem/Repositoty/IRepository/IAttendanceRecordRepository.cs
@@ -0,0 +1,13 @@
+﻿using AttendanceSystem.Models;
+using Repository.IRepository;
+
+namespace AttendanceSystem.Repositoty.IRepository
+{
+    public interface IAttendanceRecordRepository : IRepository<AttendanceRecord>
+    {
+        void Update(AttendanceRecord attendance);
+       
+
+    }
+    
+}
