639e70dd1fc74b8129d6b27040205a7de0a2bac8
doaa <doaazakaria29@gmail.com>
2024-09-24 00:47:01 +0300

    adding ILeaveRequestRepository and LeaveRequestRepository

---
+++ AttendanceSystem/Repositoty/LeaveRequestRepository.cs
@@ -0,0 +1,21 @@
+﻿using AttendanceSystem.Data;
+using AttendanceSystem.Models;
+using AttendanceSystem.Repositoty.IRepository;
+using Repository;
+
+namespace AttendanceSystem.Repositoty
+{
+    public class LeaveRequestRepository : Repository<LeaveRequest>, ILeaveRequestRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public LeaveRequestRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(LeaveRequest request)
+        {
+            _db.LeaveRequests.Update(request); ;
+        }
+    }
+}
