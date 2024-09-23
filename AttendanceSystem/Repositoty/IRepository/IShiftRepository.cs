46587b0395f7285cf142acee8390ea52777390be
doaa <doaazakaria29@gmail.com>
2024-09-24 00:47:33 +0300

    adding IShiftRepository and ShiftRepository

---
+++ AttendanceSystem/Repositoty/IRepository/IShiftRepository.cs
@@ -0,0 +1,13 @@
+﻿using AttendanceSystem.Models;
+using Repository.IRepository;
+
+namespace AttendanceSystem.Repositoty.IRepository
+{
+    public interface IShiftRepository : IRepository<Shift>
+    {
+        void Update(Shift shift);
+       
+
+    }
+    
+}
---
+++ AttendanceSystem/Repositoty/ShiftRepository.cs
@@ -0,0 +1,21 @@
+﻿using AttendanceSystem.Data;
+using AttendanceSystem.Models;
+using AttendanceSystem.Repositoty.IRepository;
+using Repository;
+
+namespace AttendanceSystem.Repositoty
+{
+    public class ShiftRepository : Repository<Shift>, IShiftRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ShiftRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Shift shift)
+        {
+            _db.Shifts.Update(shift);
+        }
+    }
+}
