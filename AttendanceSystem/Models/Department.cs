fccbae1648cad962ff30dc75339714b780796390
doaa <doaazakaria29@gmail.com>
2024-09-23 23:32:19 +0300

    add Departmentmt Model

---
+++ AttendanceSystem/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AttendanceSystem.Models
+{
+    public class Department
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string? Location { get; set; }
+       // public string Manager { get; set; }
+       public List<Employee> Employees { get; set; }
+    }
+}
