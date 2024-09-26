effb571c5a21377259f3f21dc876f7c7e8c904a1
doaa <doaazakaria29@gmail.com>
2024-09-26 22:54:02 +0300

    add DepartmentCreateDTO

---
+++ AttendanceSystem/Models/DTO/DepartmentCreateDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AttendanceSystem.Models.DTO
+{
+    public class DepartmentCreateDTO
+    {
+       
+        [Required]
+        public string Name { get; set; }
+        public string? Location { get; set; }
+    }
+}
