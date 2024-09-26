6b455d653d71d5de76475b48b72806188768b999
doaa <doaazakaria29@gmail.com>
2024-09-26 22:54:17 +0300

    add DepartmentUpdateDTO

---
+++ AttendanceSystem/Models/DTO/DepartmentUpdateDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AttendanceSystem.Models.DTO
+{
+    public class DepartmentUpdateDTO
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string? Location { get; set; }
+    }
+}
