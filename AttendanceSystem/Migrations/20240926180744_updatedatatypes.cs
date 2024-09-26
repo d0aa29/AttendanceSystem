591a80aef495011823384a18301af273c6f2c0c9
doaa <doaazakaria29@gmail.com>
2024-09-26 22:09:20 +0300

    add migration to changes

---
+++ AttendanceSystem/Migrations/20240926180744_updatedatatypes.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AttendanceSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedatatypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_AspNetUsers_UserId",
+                table: "Employees");
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "To",
+                table: "Shifts",
+                type: "time",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "From",
+                table: "Shifts",
+                type: "time",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "StartDate",
+                table: "LeaveRequests",
+                type: "time",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "EndDate",
+                table: "LeaveRequests",
+                type: "time",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RequestDate",
+                table: "LeaveRequests",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Employees",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "CheckOut",
+                table: "AttendanceRecords",
+                type: "time",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "CheckIn",
+                table: "AttendanceRecords",
+                type: "time",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_AspNetUsers_UserId",
+                table: "Employees",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_AspNetUsers_UserId",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "RequestDate",
+                table: "LeaveRequests");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "To",
+                table: "Shifts",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "From",
+                table: "Shifts",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartDate",
+                table: "LeaveRequests",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndDate",
+                table: "LeaveRequests",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Employees",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CheckOut",
+                table: "AttendanceRecords",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CheckIn",
+                table: "AttendanceRecords",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_AspNetUsers_UserId",
+                table: "Employees",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
