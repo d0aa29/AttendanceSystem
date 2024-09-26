e7c47149d158459b60702236e587560c3a075da9
doaa <doaazakaria29@gmail.com>
2024-09-26 22:56:40 +0300

    add ShiftUpdateDTO

---
+++ AttendanceSystem/Models/DTO/ShiftUpdateDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AttendanceSystem.Models.DTO
+{
+    public class ShiftUpdateDTO
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public int Num { get; set; }
+        [Required]
+        public TimeSpan From { get; set; }
+        [Required]
+        public TimeSpan To { get; set; }
+    }
+}
