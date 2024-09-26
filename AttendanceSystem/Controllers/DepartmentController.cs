a9488752f25cbe62c3564fe689d2b794d8cf81c0
doaa <doaazakaria29@gmail.com>
2024-09-26 22:55:43 +0300

    add DepartmentController for department CURD Operations

---
+++ AttendanceSystem/Controllers/DepartmentController.cs
@@ -0,0 +1,178 @@
+﻿using AttendanceSystem.Data;
+using AttendanceSystem.Models;
+using AttendanceSystem.Models.DTO;
+using AttendanceSystem.Repositoty.IRepository;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repository.IRepository;
+using System.Net;
+
+namespace AttendanceSystem.Controllers
+{
+    [Route("api/Department")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly APIResponse _response;
+        private readonly IMapper _mapper;
+        public DepartmentController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _response = new();
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<APIResponse>> GetAllDepartment()
+        {
+            try
+            {
+                IEnumerable<Department> DepartmentList = await _unitOfWork.Department.GetAll();
+                _response.Result = DepartmentList;
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [HttpGet("{id:int}", Name = "GetDepartmentById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<APIResponse>> GetDepartmentById(int id)
+        {
+            try
+            {
+                if (id == 0)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    return BadRequest(_response);
+                }
+                var dept = await _unitOfWork.Department.Get(x => x.Id == id);
+                if (dept == null)
+                {
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    return NotFound(_response);
+                }
+                // _response.Result = _mapper.Map<VillaDTO>(villa);
+                _response.Result = dept;
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<APIResponse>> CreateDepartment([FromBody] DepartmentCreateDTO DeptDTO)
+        {
+            try
+            {
+                if (DeptDTO == null)
+                    return BadRequest();
+
+                if (await  _unitOfWork.Department.Get(x => x.Name.ToLower() == DeptDTO.Name.ToLower()) != null)
+                {
+                    ModelState.AddModelError("", "Department alredy exist");
+                    return BadRequest(ModelState);
+                }
+
+                Department Dept = _mapper.Map<Department>(DeptDTO);
+
+                await _unitOfWork.Department.Create(Dept);
+                // await _dbvilla.Save();
+                _response.Result = _mapper.Map<DepartmentCreateDTO>(Dept);
+                _response.StatusCode = HttpStatusCode.OK;
+                // return Ok(_response);
+
+                return CreatedAtRoute("GetDepartmentById", new {id=Dept.Id}, _response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpDelete("{id:int}", Name = "DeleteDepartment")]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<APIResponse>> DeleteDepartment(int id)
+        {
+            try
+            {
+                if (id == null)
+                    return BadRequest();
+                var dept = await _unitOfWork.Department.Get(x => x.Id == id);
+
+                if (dept == null)
+                {
+                    return BadRequest();
+                }
+
+                await _unitOfWork.Department.Remove(dept);
+                _response.StatusCode = HttpStatusCode.NoContent;
+                _response.IsSuccess = true;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPut("{id:int}", Name = "UpdateDepartment")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<APIResponse>> UpdateDepartment(int id, [FromBody] DepartmentUpdateDTO DeptDTO)
+        {
+            try
+            {
+                if (id != DeptDTO.Id || DeptDTO == null)
+                    return BadRequest();
+
+
+                Department model = _mapper.Map<Department>(DeptDTO);
+
+
+                await _unitOfWork.Department.Update(model);
+
+                //  _response.Result = _mapper.Map<VillaDTO>(model);
+                _response.StatusCode = HttpStatusCode.NoContent;
+                _response.IsSuccess = true;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+    }
+}
