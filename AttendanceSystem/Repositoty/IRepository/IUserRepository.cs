a56b28e0a281036d33ebcf387ff0a92a61374b8c
doaa <doaazakaria29@gmail.com>
2024-09-27 23:52:01 +0300

    add IUserRepository and UserRepository to manage users of the system

---
+++ AttendanceSystem/Repositoty/IRepository/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using AttendanceSystem.Models;
+using AttendanceSystem.Models.DTO;
+using Repository.IRepository;
+using System.Linq.Expressions;
+
+
+namespace AttendanceSystem.Repositoty.IRepository {
+    public interface IUserRepository :IRepository<ApplicationUser>
+    {
+        Task Update(ApplicationUser user);
+       
+    }
+
+}
+
---
+++ AttendanceSystem/Repositoty/UserRepository.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using AttendanceSystem.Data;
+using AttendanceSystem.Models;
+using AttendanceSystem.Models.DTO;
+//using AttendanceSystem.Repository.IRepository;
+using System.Data;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System.Linq;
+using Repository;
+using AttendanceSystem.Repositoty.IRepository;
+
+namespace AttendanceSystem.Repository
+{
+    public class UserRepository :  Repository<ApplicationUser>, IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public UserRepository(ApplicationDbContext db) : base(db)
+        {
+          
+            _db = db;
+        }
+       
+        public async Task Update(ApplicationUser user)
+        {
+            _db.ApplicationUsers.Update(user);
+            await _db.SaveChangesAsync();
+        }
+
+       
+    }
+}
