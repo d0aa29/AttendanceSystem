1487a4ab0e3787c47164bd87536d55773df0b267
doaa <doaazakaria29@gmail.com>
2024-09-28 00:46:01 +0300

    add DTO Models that required for employee create and update

---
+++ AttendanceSystem/Models/DTO/EmployeeUpdateDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AttendanceSystem.Models.DTO
+{
+    public class EmployeeUpdateDTO
+    {
+        public int Id { get; set; }
+        public int SSN { get; set; }
+        public string Name { get; set; }
+        public string? Gender { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public DateTime? JoinedOn { get; set; }
+        public int? DepartmentId { get; set; }
+        public string? UserId { get; set; }
+    }
+}
