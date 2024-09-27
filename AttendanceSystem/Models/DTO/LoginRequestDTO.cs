92d9e77c3ac2a94b0ddc2893082e0551c3fcc552
doaa <doaazakaria29@gmail.com>
2024-09-28 00:03:00 +0300

    add all DTO models required for Login

---
+++ AttendanceSystem/Models/DTO/LoginRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace AttendanceSystem.Models.DTO
+{
+    public class LoginResponseDTO
+    {
+        public UserDTO User { get; set; }
+        
+     //   public string Role { get; set; }
+        public string Token { get; set; }
+    }
+}
