5ed6168b460ed991140afdce7a2b63d3e010e80b
doaa <doaazakaria29@gmail.com>
2024-09-27 23:50:20 +0300

    add UserDTO and UserUpdateDTO to DTOModels

---
+++ AttendanceSystem/Models/DTO/UserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace AttendanceSystem.Models.DTO
+{
+    public class UserDTO
+    {
+       // public int ID { get; set; }
+      // public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ AttendanceSystem/Models/DTO/UserUpdateDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace AttendanceSystem.Models.DTO
+{
+    public class UserUpdateDTO
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+       // public string Name { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
