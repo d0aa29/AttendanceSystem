af33e57cd924057ab58d0cf4f655c1f01395773e
doaa <doaazakaria29@gmail.com>
2024-09-28 00:05:18 +0300

    add AuthenticationController

---
+++ AttendanceSystem/Controllers/AuthenticationController.cs
@@ -0,0 +1,84 @@
+﻿using AttendanceSystem.Models;
+using AttendanceSystem.Models.DTO;
+using AttendanceSystem.Repositoty.IRepository;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Repository;
+using Repository.IRepository;
+using System.Net;
+
+namespace AttendanceSystem.Controllers
+{
+    [Route("api/Authentication")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IAthuRepository _auth;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        protected APIResponse _response;
+        private readonly IMapper _mapper;
+        public AuthenticationController(IAthuRepository auth, IMapper mapper, UserManager<ApplicationUser> userManager
+            , RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+            _roleManager = roleManager;
+            //  _userRepository = userRepository;
+            _auth = auth;
+            this._response = new();
+        }
+
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterationRequestDTO registerationRequestDTO)
+        {
+            bool ifUserNameUnique = _auth.IsUniqueUser(registerationRequestDTO.UserName);
+            if (!ifUserNameUnique)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessages.Add("username already exists");
+                return BadRequest(_response);
+            }
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            // Call the Register method in the repository
+            var user = await _auth.Register(registerationRequestDTO);
+
+            if (user == null)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessages.Add("Error while registering");
+                return BadRequest(_response);
+            }
+            _response.StatusCode = HttpStatusCode.OK;
+            _response.IsSuccess = true;
+
+            return Ok(_response);
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDTO model)
+        {
+            var loginResponse = await _auth.Login(model);
+            if (loginResponse.User == null || string.IsNullOrEmpty(loginResponse.Token))
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessages.Add("username or password is incorrect");
+                return BadRequest(_response);
+            }
+            _response.StatusCode = HttpStatusCode.OK;
+            _response.IsSuccess = true;
+            _response.Result = loginResponse;
+            return Ok(_response);
+        }
+
+    }
+}
