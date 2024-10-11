b3b2517db90b12a86fcbf75ec0fe54ccbe5e1a45
doaa <doaazakaria29@gmail.com>
2024-10-11 10:19:19 +0300

    add AttendanceRecordController

---
+++ AttendanceSystem/Controllers/AttendanceRecordController.cs
@@ -0,0 +1,28 @@
+﻿using AttendanceSystem.Models;
+using AttendanceSystem.Models.DTO;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repository.IRepository;
+using System.Net;
+using System.Security.Claims;
+
+namespace AttendanceSystem.Controllers
+{
+    [Route("api/AttendanceRecord")]
+    [ApiController]
+    public class AttendanceRecordController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly APIResponse _response;
+        private readonly IMapper _mapper;
+        public AttendanceRecordController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _response = new();
+            _mapper = mapper;
+        }
+     
+    }
+}
