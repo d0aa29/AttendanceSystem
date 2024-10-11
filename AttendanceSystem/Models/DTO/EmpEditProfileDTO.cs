05406b0f7aa3d88b79c7d5205ccd7c2b6e65c655
doaa <doaazakaria29@gmail.com>
2024-10-11 09:54:18 +0300

    add  EmpEditProfileDTO

---
+++ AttendanceSystem/Models/DTO/EmpEditProfileDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace AttendanceSystem.Models.DTO
+{
+    public class EmpEditProfileDTO
+    {
+        public int SSN { get; set; }
+        public string Name { get; set; }
+        public string? Gender { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public DateTime? JoinedOn { get; set; }
+        public int? DepartmentId { get; set; }
+    }
+}
