b89350fd2d749d898b477e5f40009122f3fb0137
doaa <doaazakaria29@gmail.com>
2024-10-11 09:41:31 +0300

    add Migration updateApplicationUser

---
+++ AttendanceSystem/Migrations/20240926232216_updateApplicationUser.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace AttendanceSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateApplicationUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "1", null, "Admin", "ADMIN" },
+                    { "2", null, "Employee", "EMPLOYEE" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[,]
+                {
+                    { "1", 0, "31bcad75-18fa-42da-bbf1-4bc7db9b27a8", "admin@system.com", true, false, null, "ADMIN@SYSTEM.COM", "ADMIN@SYSTEM.COM", "AQAAAAIAAYagAAAAEMw2Guftk+5pe0A/puMbcg6bHFOiw8h1quIZ4xOBB/ypZW+tHMvgALsBe0TY13tJwQ==", null, false, "9fd72d50-6c1e-479d-9f69-50b45e5e4baf", false, "admin@system.com" },
+                    { "2", 0, "1f63b522-25cc-4e84-8df8-d5fb716f716c", "jane@system.com", true, false, null, "JANE@SYSTEM.COM", "JANE@SYSTEM.COM", "AQAAAAIAAYagAAAAENqjgqWOOA0rVJYSthr2DrF6Hh0bNU7pzOQXezZv7zBSxkq1xrw0GE8PTDHl590StQ==", null, false, "d355bddb-be8a-420a-ba6e-44fa903ec592", false, "jane@system.com" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "UserId",
+                value: "2");
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[,]
+                {
+                    { "1", "1" },
+                    { "2", "2" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "1", "1" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "2", "2" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "1");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "2");
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "UserId",
+                value: null);
+        }
+    }
+}
---
+++ AttendanceSystem/Migrations/20241002083004_updateAttendanceRecordTable.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AttendanceSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateAttendanceRecordTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Reason",
+                table: "LeaveRequests",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ShiftId",
+                table: "AttendanceRecords",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "1",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "8c537f33-1289-461c-8269-60512da32c04", "AQAAAAIAAYagAAAAENmtWmLcQbZ/fBkAr3da6DwDSgep6ZCyuRfiYWevFSUGsuPuOPnXFjr6DZ1G2skWMg==", "e3276da3-501a-48d2-8ca1-cfef0a96897e" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "2",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "66cb55e0-016d-4656-8537-e1619a01fb65", "AQAAAAIAAYagAAAAELWaIR5NaVUCmkX2nasyLSRKQXKHaB5yYA+/ehMUw+EnnvWtoFfsoteXM/wkSoIgXg==", "b4d43612-2a33-4648-a8c1-af912fda03e2" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AttendanceRecords_ShiftId",
+                table: "AttendanceRecords",
+                column: "ShiftId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AttendanceRecords_Shifts_ShiftId",
+                table: "AttendanceRecords",
+                column: "ShiftId",
+                principalTable: "Shifts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AttendanceRecords_Shifts_ShiftId",
+                table: "AttendanceRecords");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AttendanceRecords_ShiftId",
+                table: "AttendanceRecords");
+
+            migrationBuilder.DropColumn(
+                name: "ShiftId",
+                table: "AttendanceRecords");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Reason",
+                table: "LeaveRequests",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "1",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "31bcad75-18fa-42da-bbf1-4bc7db9b27a8", "AQAAAAIAAYagAAAAEMw2Guftk+5pe0A/puMbcg6bHFOiw8h1quIZ4xOBB/ypZW+tHMvgALsBe0TY13tJwQ==", "9fd72d50-6c1e-479d-9f69-50b45e5e4baf" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "2",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "1f63b522-25cc-4e84-8df8-d5fb716f716c", "AQAAAAIAAYagAAAAENqjgqWOOA0rVJYSthr2DrF6Hh0bNU7pzOQXezZv7zBSxkq1xrw0GE8PTDHl590StQ==", "d355bddb-be8a-420a-ba6e-44fa903ec592" });
+        }
+    }
+}
