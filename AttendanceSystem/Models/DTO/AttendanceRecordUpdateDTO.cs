79d55d954b6322476754b2db8e122837ccc76c9e
doaa <doaazakaria29@gmail.com>
2024-10-11 10:15:26 +0300

    add AttendanceRecordUpdateDTO

---
+++ AttendanceSystem/Models/DTO/AttendanceRecordUpdateDTO.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AttendanceSystem.Models
+{
+    public class AttendanceRecordUpdateDTO
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+      //  public DateTime? Date { get; set; }= DateTime.Now;
+       // public TimeSpan CheckIn { get; set; }= DateTime.UtcNow.TimeOfDay;
+      //  public TimeSpan CheckOut { get; set; } = DateTime.UtcNow.TimeOfDay;
+       // public string OutStatus { get; set; }
+     //   public string InStatus { get; set; }
+        public string? Note { get; set; }
+
+        //[Required]
+        //public int EmployeeId { get; set; }
+        //[ForeignKey("EmployeeId")]
+        //public Employee Employee { get; set; }
+
+        //[Required]
+        //public int ShiftId { get; set; } 
+
+        //[ForeignKey("ShiftId")]
+        //public Shift Shift { get; set; }  
+    }
+}
