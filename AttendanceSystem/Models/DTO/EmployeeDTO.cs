0d20846c1a124fa9590aa96570f1e0aa1220524f
doaa <doaazakaria29@gmail.com>
2024-10-11 09:53:04 +0300

    add  EmployeeDTO

---
+++ AttendanceSystem/Models/DTO/EmployeeDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AttendanceSystem.Models.DTO
+{
+    public class EmployeeDTO
+    {
+        public int Id { get; set; }
+        public int SSN { get; set; }
+        public string Name { get; set; }
+        public string? Gender { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public DateTime? JoinedOn { get; set; }
+        public string? DepartmentName { get; set; }
+        public string? UserName { get; set; }
+        public List<ShiftDTO>? Shifts { get; set; } = new List<ShiftDTO>();
+    }
+}
