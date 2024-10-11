e05a680829fc8848e88fd3f3ada0d3c92992475d
doaa <doaazakaria29@gmail.com>
2024-10-11 08:58:47 +0300

    adding LeaveRequestController

---
+++ AttendanceSystem/Controllers/LeaveRequestController.cs
@@ -0,0 +1,31 @@
+﻿using AttendanceSystem.Data;
+using AttendanceSystem.Models;
+using AttendanceSystem.Models.DTO;
+using AttendanceSystem.Repositoty.IRepository;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repository.IRepository;
+using System.Net;
+using System.Security.Claims;
+
+namespace AttendanceSystem.Controllers
+{
+    [Route("api/Request")]
+    [ApiController]
+    public class LeaveRequestController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly APIResponse _response;
+        private readonly IMapper _mapper;
+        public LeaveRequestController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _response = new();
+            _mapper = mapper;
+        }
+
+       
+    }
+}
