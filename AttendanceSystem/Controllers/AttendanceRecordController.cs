088bc540f5f71cde1edcfdb1470f1e2b9b830f45
doaa <doaazakaria29@gmail.com>
2024-10-11 10:20:30 +0300

    add GetAllEmployeeRecords action in AttendanceRecordController

--- AttendanceSystem/Controllers/AttendanceRecordController.cs
+++ AttendanceSystem/Controllers/AttendanceRecordController.cs
@@ -23,6 +23,58 @@ namespace AttendanceSystem.Controllers
             _response = new();
             _mapper = mapper;
         }
+        [HttpGet("AllAttendanceRecord")]
+        [Authorize(Roles = " Admin, Manager")]
+        public async Task<ActionResult<APIResponse>> GetAllEmployeeRecords()
+        {
+            try
+            {
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+
+                var employee = await _unitOfWork.Employee.Get(u => u.UserId == userId);
+                //  var reqcordList = await _unitOfWork.AttendanceRecord.GetAll(u => u.EmployeeId == employee.Id, false, includProperties: "Employee");
+
+
+                if (employee == null)
+                {
+                    return NotFound();
+                }
+                // IEnumerable<AttendanceRecord>
+                // var reqcordList = await _unitOfWork.AttendanceRecord.GetAll(x => x.EmployeeId == employee.Id);
+                IEnumerable<AttendanceRecord> reqcordList;
+
+                // Check if the user is an admin or a manager
+                if (User.IsInRole("Admin"))
+                {
+                    // Admin can access all attendance records
+                    reqcordList = await _unitOfWork.AttendanceRecord.GetAll(includProperties: "Employee");
+                }
+                else if (User.IsInRole("Manager"))
+                {
+                    // Manager can only access attendance records for employees in their department
+                    reqcordList = await _unitOfWork.AttendanceRecord.GetAll(x => x.Employee.DepartmentId == employee.DepartmentId, includProperties: "Employee");
+                }
+                else
+                {
+
+                    return Forbid();
+                }
+                var ReqcordListDTO = _mapper.Map<IEnumerable<AttendanceRecordDTO>>(reqcordList);
+                _response.Result = ReqcordListDTO;
+
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
      
+
     }
 }
