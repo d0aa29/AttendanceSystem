02919ce30eb5224731774f83a53be1178545cbc1
doaa <doaazakaria29@gmail.com>
2024-10-11 10:15:09 +0300

    add AttendanceRecordCreateDTO

---
+++ AttendanceSystem/Models/DTO/AttendanceRecordCreateDTO.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AttendanceSystem.Models
+{
+    public class AttendanceRecordCreateDTO
+    {
+        //[Key]
+        //[Required]
+        //public int Id { get; set; }
+       
+       // public TimeSpan CheckOut { get; set; }
+      //  public string OutStatus { get; set; }
+      //  public string InStatus { get; set; }
+        public string? Note { get; set; }
+
+        //[Required]
+        //public int EmployeeId { get; set; }
+        //[ForeignKey("EmployeeId")]
+        //public Employee Employee { get; set; }
+
+        [Required]
+        public int ShiftId { get; set; } 
+
+     
+    }
+}
